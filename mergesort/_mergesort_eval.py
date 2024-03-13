4c4d4c2b0c04b5a77d794aafe858f15b0bba180a
Kyle Montgomery <kylemontgomery@wustl.edu>
2024-03-13 12:47:36 -0500

    working on eval

--- mergesort/_mergesort_eval.py
+++ mergesort/_mergesort_eval.py
@@ -10,17 +10,18 @@ import ast
 import re
 import argparse
 import json
+import math
 
 def init_model(lora_dir):
-    model = AutoModelForCausalLM.from_pretrained('huggyllama/llama-7b',cache_dir='/scratch/kyle/LLMDivideAndConquer/cache/kylemontgomery', torch_dtype=torch.float16, device_map='auto')
+    model = AutoModelForCausalLM.from_pretrained('huggyllama/llama-7b',cache_dir='/scratch/kyle/LLMDivideAndConquer/cache', torch_dtype=torch.float16, device_map='auto')
     model = PeftModel.from_pretrained(model, lora_dir)
     tokenizer = AutoTokenizer.from_pretrained("huggyllama/llama-7b")
     if tokenizer.pad_token_id is None:
         tokenizer.pad_token_id = tokenizer.eos_token_id
     return model, tokenizer
 
-def generate(model, tokenizer, prompt, max_length, stop=None):
-    input_tok=tokenizer(prompt,add_special_tokens=False,padding=True)
+def generate(model, tokenizer, prompt, max_length):
+    input_tok=tokenizer([prompt],add_special_tokens=False,padding=True)
     input_ids=torch.LongTensor(input_tok['input_ids']).cuda()
     input_length = input_ids.shape[1]
     attention_mask=torch.LongTensor(input_tok['attention_mask']).cuda()
@@ -32,23 +33,28 @@ def generate(model, tokenizer, prompt, max_length, stop=None):
         pad_token_id=tokenizer.pad_token_id
     )
     generated_text = tokenizer.batch_decode(tokenized_samples,skip_special_tokens=True)[0]
-    last_idx = min([generated_text.find(s, input_length) if generated_text.find(s, input_length) > input_length else 1000000000 for s in stop])
-    return generated_text[:last_idx+1]
+    return generated_text
 
 
 def recursive_generate(model, tokenizer, prompt, max_length, max_depth):
     if max_depth == 0:
         raise ValueError("Max depth reached")
-    output = generate(model, tokenizer, prompt, max_length, stop=["="])
+    output = generate(model, tokenizer, prompt, max_length)
     sort_matches = re.findall(r'sort\(\[[^\]]+\]\)', output)
     if len(sort_matches)<2:
+        print(output)
         return output
     for match in sort_matches[1:]:
         sorted_result = recursive_generate(model, tokenizer, f"{match} = ", max_length, max_depth-1)
-        output = output.replace(match, sorted_result)
-    output = generate(model, tokenizer, f"{output} = ", max_length, stop=["]"])
+        output = output.replace(match, parse_last_list(sorted_result))
+    output = generate(model, tokenizer, f"{output} = ", max_length)
+    print(output)
     return output
-        
+
+
+def parse_last_list(s):
+    matches =  re.findall(r'\[[^\]]*\]', s)
+    return matches[-1] if matches else None
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -66,7 +72,7 @@ if __name__ == "__main__":
         for i in data.keys():
             count = 0
             for j in range(len(data[i])):
-                pred = generate(model, tokenizer, f"{data[i][j]['input']}", 2048, stop = ["]"]).split("=")[-1].strip().rstrip()
+                pred = generate(model, tokenizer, f"{data[i][j]['input']}", 2048).split("=")[-1].strip().rstrip()
                 gold = data[i][j]['output']
                 if pred == gold:
                     count += 1
@@ -76,7 +82,7 @@ if __name__ == "__main__":
         for i in data.keys():
             count = 0
             for j in range(len(data[i])):
-                pred = recursive_generate(model, tokenizer, f"{data[i][j]['input']}", 2048, i)[0].split("=")[-1].strip().rstrip()
+                pred = recursive_generate(model, tokenizer, f"{data[i][j]['input']}", 2048, math.ceil(math.log2(int(i))) + 1)[0].split("=")[-1].strip().rstrip()
                 gold = data[i][j]['output']
                 if pred == gold:
                     count += 1
